87abf43bcb3e843bbe0604a037c04826fac734ce
Sonu Mor <SMor@radiuspointhq.local>
2022-07-02 16:03:09 -0400

    Add project files.

---
+++ Test2022/Controllers/RegisterUserTask5Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test2022.Controllers
+{
+    public class RegisterUserTask5Controller : Controller
+    {
+        // GET: RegisterUserTask5
+        public ActionResult RegisterUserTask5View()
+        {
+            return View();
+        }
+    }
+}
