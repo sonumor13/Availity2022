0731cce66becc2888617fe3f784fe2e3fe13fbf4
Sonu Mor <SMor@radiuspointhq.local>
2022-07-02 16:03:09 -0400

    Add project files.

---
+++ Test2022/Controllers/ValidateparenthesesTask4Controller.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test2022.Controllers
+{
+    public class ValidateparenthesesTask4Controller : Controller
+    {
+        // GET: ValidateparenthesesTask4
+        public ActionResult ValidateparenthesesTask4View()
+        {
+            return View();
+        }
+        public ActionResult ValidateInput(string LISPCode)
+        {
+            int Num = CheckInputFile(LISPCode);
+
+            if (Num == 0) return Json("Failed", JsonRequestBehavior.AllowGet);
+            else return Json("Success", JsonRequestBehavior.AllowGet);
+
+        }
+
+        public int CheckInputFile(string LISPCode)
+        {
+
+            // Return 1 if string size is 0 
+            if (LISPCode.Length == 0) return 1;
+
+            Stack<char> brackets = new Stack<char>();
+
+            foreach (char c in LISPCode)
+            {
+                if (c == '[' || c == '{' || c == '(')
+                {
+                    brackets.Push(c);
+                }
+                else
+                {                    
+                        if (c == ')')
+                        {
+                            if (brackets.Count > 0) // check if the stack count is greater then 0
+                            {
+                                if (brackets.Peek() == '(') brackets.Pop();
+                                else return 0;
+                            }
+                             else return 0;
+                        }
+
+                        if (c == '}')
+                        {
+                            if (brackets.Count > 0) // check if the stack count is greater then 0
+                            {
+                                if (brackets.Peek() == '{') brackets.Pop();
+                                else return 0;
+                            }
+
+                            else return 0;
+                        }
+
+                        if (c == ']')
+                        {
+                                if (brackets.Count > 0) // check if the stack count is greater then 0
+                                {
+                                    if (brackets.Peek() == '[') brackets.Pop();
+                                else return 0;
+                            }
+                            else return 0;
+                        }
+                   
+
+                }
+            }
+
+            // if there are no parantheses in the stack then it is valid string
+            if (brackets.Count == 0) return 1;
+
+            return 0;
+        }
+    }
+}
---
+++ Test2022/Models/AssignmentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test2022.Models
+{
+    public class AssignmentModel
+    {
+        public HttpPostedFileBase file { get; set; }
+
+        public string FirsLastName { get; set; }
+        public string NPINumber { get; set; }
+        public string BusinesAddress { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+}
